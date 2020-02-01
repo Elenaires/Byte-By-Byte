24a3561faedbc2821dd24ac47aba9af0a3b0633a
Lee Huey Charn <hueycharn.lee@gmail.com>
2020-02-01 21:13:04 +1100

    Qs3

--- MatrixProduct.java
+++ MatrixProduct.java
@@ -1,34 +1,44 @@
 //Question:​ Given a matrix, find the path from top left to bottom right with the greatest product by moving only down and right.
-
+import java.util.*;
 public class MatrixProduct 
 {
 	static int maxValue = 0;
 	public static void main(String[] args)
 	{
-		int[][] matrix = new int[][]{{-1,2,3},{4,5,-6},{7,8,100}};
+		int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+		List<Integer> list = new ArrayList<>();
+		List<Integer> result = new ArrayList<>();
+		dfs(matrix, 1, 0, 0, list, result);
 		
-		dfs(matrix, 1, 0, 0);
-		
-		System.out.println(maxValue);		
+		System.out.println(maxValue);
+		for(int k = 0; k < result.size(); k++) {
+			System.out.print(result.get(k) + " ");
+		}
+		System.out.println();
 	}
 
-	public static void dfs(int[][] array, int max, int i, int j) {
+	public static void dfs(int[][] array, int max, int i, int j, List<Integer> list, List<Integer> result) {
 		int[][] shift = new int[][]{{1,0}, {0,1}};
+		max *= array[i][j];
+		list.add(array[i][j]);
 
 		if(i == array.length-1 && j == array[0].length-1) {
-			max *= array[i][j];
-			maxValue = Math.max(maxValue, max);
+			if(maxValue < max) {
+				maxValue = max;
+				result.clear();
+				result.addAll(list);
+			}
 		}
 		else {
-			max *= array[i][j];
 			for(int[] s : shift) {
 				int newI = i + s[0];
 				int newJ = j + s[1];
 				if(isFeasible(array, newI, newJ)) {
-					dfs(array, max, newI, newJ);
+					dfs(array, max, newI, newJ, list, result);
 				}
 			}
 		}
+		list.remove(list.size()-1);
 	}
 
 	private static boolean isFeasible(int[][] array, int i, int j) {
